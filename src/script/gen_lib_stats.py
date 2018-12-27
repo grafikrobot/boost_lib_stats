f78a96565b5b71928941a8a6035531a4a0677047
Rene Rivera <grafikrobot@gmail.com>
2018-12-27 13:25:14 -0600

    Add cycles for build/sources.

--- src/script/gen_lib_stats.py
+++ src/script/gen_lib_stats.py
@@ -17,12 +17,15 @@ class GneLibStats(Main):
     def __init_parser__(self, parser):
         parser.add_argument('++data-dir')
         parser.add_argument('++versions')
-        parser.add_argument('++cycles-table-json')
+        parser.add_argument('++json')
+        parser.add_argument('++kind')
+        parser.add_argument('++headers', action='store_true', default=True)
+        parser.add_argument('++build', action='store_true', default=False)
 
     def __run__(self):
-        lib_data_headers = []
-        lib_data_build = []
-        lib_data_versions = []
+        self.lib_data_headers = []
+        self.lib_data_build = []
+        self.lib_data_versions = []
         with PushDir(self.args.data_dir):
             for v in self.args.versions.split(','):
                 v = v.split('-')
@@ -34,29 +37,34 @@ class GneLibStats(Main):
                         data_build = LibraryData(self.args)
                         data_build.load_ranks_info(
                             'boost-1.%s.0-ranks-build.json' % (r))
-                        lib_data_versions.append('1.%s.0' % (r))
-                        lib_data_headers.append(data_headers.ranks_info)
-                        lib_data_build.append(data_build.ranks_info)
+                        self.lib_data_versions.append('1.%s.0' % (r))
+                        self.lib_data_headers.append(data_headers.ranks_info)
+                        self.lib_data_build.append(data_build.ranks_info)
                 else:
                     data_headers = LibraryData(self.args)
                     data_headers.load_ranks_info(
                         '%s-ranks-headers.json' % (v[0]))
                     data_build = LibraryData(self.args)
                     data_build.load_ranks_info('%s-ranks-build.json' % (v[0]))
-                    lib_data_versions.append(v[0])
-                    lib_data_headers.append(data_headers.ranks_info)
-                    lib_data_build.append(data_build.ranks_info)
-        lib_stats_headers = LibraryStats(self.args)
+                    self.lib_data_versions.append(v[0])
+                    self.lib_data_headers.append(data_headers.ranks_info)
+                    self.lib_data_build.append(data_build.ranks_info)
 
-        if self.args.cycles_table_json:
-            lib_stats_headers.gen_cycles_table(lib_data_headers,
-                                               lib_data_versions)
-            with open(self.args.cycles_table_json, "w") as f:
+        getattr(self, '__gen_%s__' % (self.args.kind))()
+
+    def __gen_cycles_table__(self):
+        lib_stats = LibraryStats(self.args)
+        if self.args.json:
+            if self.args.headers:
+                lib_stats.gen_cycles_table(self.lib_data_headers,
+                                           self.lib_data_versions)
+            else:
+                lib_stats.gen_cycles_table(self.lib_data_build,
+                                           self.lib_data_versions)
+            with open(self.args.json, "w") as f:
                 f.write(
                     json.dumps(
-                        lib_stats_headers.cycles_table,
-                        sort_keys=True,
-                        indent=4))
+                        lib_stats.cycles_table, sort_keys=True, indent=4))
 
 
 if __name__ == "__main__":
