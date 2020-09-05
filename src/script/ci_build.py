17fe38e73b510d0b79e5fc83cb769cdc6b1fb473
Rene Rivera <grafikrobot@gmail.com>
2020-09-04 20:23:05 -0500

    Add Boost 1.74

--- src/script/ci_build.py
+++ src/script/ci_build.py
@@ -95,7 +95,7 @@ class CIBuild(Main):
                         '++json=%s' % (ranks_build_file), '++buildable'
                     ])
 
-            version_range = (57, 73)
+            version_range = (57, 74)
 
             if self.args.build_data:
                 gen_lib_data(branch='develop', rebuild=True)
